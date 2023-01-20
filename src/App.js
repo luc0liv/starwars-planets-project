f2ff64747f61393dd77a95821714136a33e9374f
luc0liv <luoliveiradacunha@gmail.com>
2023-01-20 19:08:24 -0300

    REQ 1: Requisição a API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Table from './components/Table';
+import PlanetsContext from './context/PlanetsContext';
 
 function App() {
+  const [planets, setPlanets] = useState([]);
+
+  useEffect(() => {
+    fetch('https://swapi.dev/api/planets')
+      .then((res) => res.json())
+      .then((res) => {
+        const newPlanetsArray = res.results.map(({ residents, ...planet }) => planet);
+        setPlanets(newPlanetsArray);
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
   return (
-    <span>Star Wars</span>
+    <PlanetsContext.Provider value={ { planets } }>
+      <Table />
+    </PlanetsContext.Provider>
   );
 }
 
