79572794353875f05c9ff0bcd25c73be587ed637
luc0liv <luoliveiradacunha@gmail.com>
2023-01-24 21:49:57 -0300

    REQ 8

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render, screen, act } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 import testData from '../../cypress/mocks/testData';
 
@@ -12,10 +13,23 @@ it('teste se os elementos iniciais são renderizados', () => {
   const filterButton = screen.getByRole('button', {
     name: /filtrar/i
   });
+  const columnFilter = screen.getByTestId('column-filter');
+  const comparisonFilter = screen.getByTestId('comparison-filter');
+  const valueFilter = screen.getByTestId('value-filter');
   const removeAllFiltersButton = screen.getByRole('button', {
     name: /remover todos os filtros/i
   });
+
   expect(nameInputElement).toBeInTheDocument();
+  expect(nameInputElement).toHaveValue('');
+  expect(columnFilter).toBeInTheDocument();
+  expect(columnFilter.children).toHaveLength(5);
+  expect(columnFilter).toHaveValue('population');
+  expect(comparisonFilter).toBeInTheDocument();
+  expect(comparisonFilter.children).toHaveLength(3);
+  expect(comparisonFilter).toHaveValue('maior que');
+  expect(valueFilter).toBeInTheDocument();
+  expect(valueFilter).toHaveValue(0);
   expect(filterButton).toBeInTheDocument();
   expect(removeAllFiltersButton).toBeInTheDocument();
 });
@@ -26,6 +40,7 @@ it('teste se a API foi chamada', async () => {
     json: jest.fn().mockResolvedValue(testData),
   });
   await act( async () => render(<App/>));
+
   expect(global.fetch).toHaveBeenCalled();
   expect(global.fetch).toHaveBeenCalledTimes(1);
   expect(global.fetch).toHaveBeenCalledWith('https://swapi.dev/api/planets');
@@ -36,4 +51,51 @@ it('teste se a API foi chamada', async () => {
   expect(nameHeader).toBeInTheDocument();
   const tableHeader = screen.getAllByRole('columnheader');
   expect(tableHeader).toHaveLength(13);
-})
+  const tatooine = screen.getByRole('cell', {  name: /tatooine/i});
+  expect(tatooine).toBeInTheDocument();
+});
+
+it('teste se ao digitar nome a tabela é atualizada', async () => {
+  jest.spyOn(global, 'fetch');
+  global.fetch.mockResolvedValue({
+    json: jest.fn().mockResolvedValue(testData),
+  });
+  await act( async () => render(<App/>));
+
+  const nameInput = screen.getByTestId('name-filter');
+  userEvent.type(nameInput, 'tatooine');
+
+  const tableRows = screen.getAllByRole('row');
+  expect(tableRows).toHaveLength(2);
+
+  const tatooine = screen.getByRole('cell', {  name: /tatooine/i});
+  expect(tatooine).toBeInTheDocument();
+
+  userEvent.clear(nameInput);
+  const newTableRows = screen.getAllByRole('row');
+  expect(newTableRows).toHaveLength(11);
+
+  userEvent.type(nameInput, 'oo');
+  expect(screen.getAllByRole('row')).toHaveLength(3);
+});
+
+it('teste adicionando e removendo um filtro', async () => {
+  jest.spyOn(global, 'fetch');
+  global.fetch.mockResolvedValue({
+    json: jest.fn().mockResolvedValue(testData),
+  });
+  await act( async () => render(<App/>));
+  const filterButton = screen.getByRole('button', {
+    name: /filtrar/i
+  });
+
+  userEvent.click(filterButton);
+  expect(screen.getAllByRole('row')).toHaveLength(9);
+
+  const filter = screen.getByRole('button', {  name: /population maior que 0 lixeira/i});
+  expect(filter).toBeInTheDocument();
+
+  userEvent.click(filter);
+  expect(screen.getAllByRole('row')).toHaveLength(11);
+});
+
